c6557c1e3a76bf7fe1317ffd8b92fff9c85597c6
hajerhajj <128532009+hajerhajj@users.noreply.github.com>
2024-06-04 09:33:45 +0100

    Update settings.py

--- Dash/settings.py
+++ Dash/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'dashb',
         'USER': 'postgres',
         'PASSWORD': '139',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
